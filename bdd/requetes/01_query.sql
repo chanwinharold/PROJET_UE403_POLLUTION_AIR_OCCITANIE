4cc68cfc0800110c50c5a7965ec35ef457588755
lucaHirche <lucamouissi@icloud.com>
2026-03-13 14:23:19 +0100

    ajout d'attribut

--- bdd/requetes/01_query.sql
+++ bdd/requetes/01_query.sql
@@ -1,4 +1,4 @@
-SELECT  c.code_insee_com, c.nom_com, cl.RR_med, cl.NBJRR1_med,cl.NBJRR10_med,cl.Tmin_med,cl.Tmax_med,cl.Tens_vap_med,cl.Force_vent_med,
+SELECT  c.code_insee_com, c.nom_com, cl.RR_med,NBJRR5_med,cl.NBJRR1_med,cl.NBJRR10_med,cl.Tmin_med,cl.Tmax_med,cl.Tens_vap_med,cl.Force_vent_med,
 cl.Insolation_med,cl.Rayonnement_med,se.niveau_vie_median_2021
 FROM UE403_COMMUNES  c
 JOIN UE403_GEO_CLIMATIQUES  cl 
